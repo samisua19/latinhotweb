c494de01be85a64f10af82f63f3b943153dc06f1
Larozox <samisua16@gmail.com>
2022-07-27 22:22:28 -0500

    conexion a firebase

---
+++ src/database/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPcqI5CQLSStm1UV7HjN_WF7iNXWt-tzQ",
+  authDomain: "latinhotweb-2563b.firebaseapp.com",
+  projectId: "latinhotweb-2563b",
+  storageBucket: "latinhotweb-2563b.appspot.com",
+  messagingSenderId: "1070772122087",
+  appId: "1:1070772122087:web:718207105f165d81bd8d42",
+  measurementId: "G-8WS5TS5KR0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app);
+const analytics = getAnalytics(app)

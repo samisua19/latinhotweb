3693e1333e38b1b6ce661a1cd035ec494940ee19
Samuel Suarez <sdsuarez@saludelectronica.com>
2022-08-07 00:31:20 -0500

    config route

--- src/components/modalComponent.js
+++ src/components/modalComponent.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import {Button, Modal, Form} from 'react-bootstrap';
-import { Navigate, useH } from "react-router-dom"
+import React, { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default () => {
   const [show, setShow] = useState(false);
-  const [pass, setPass] = useState("")
+  const [pass, setPass] = useState("");
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  let navigate = useNavigate();
 
   const changeForm = (event) => {
-    setPass(event.target.value)
-  }
+    setPass(event.target.value);
+  };
 
-  const handleFormGirl = (event) =>{
-    event.preventDefault()
-    localStorage.setItem('pass', pass)
+  const handleFormGirl = (event) => {
+    event.preventDefault();
+    localStorage.setItem("pass", pass);
     const url = window.location.href
-    window.location.assign(url + "createGirl")
-  }
+    return navigate("/createGirl")
+  };
 
   return (
     <>
@@ -35,16 +36,22 @@ export default () => {
           <Modal.Title>Autorización para crear</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form onSubmit={handleFormGirl} id="formGirls">
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control required={true} name='password' type="password" placeholder="Contraseña" onChange={changeForm} />
-          </Form.Group>
-          <div className="d-grid gap-2">
-            <Button variant="danger" type="submit">
-              Entrar
-            </Button>
-          </div>
-        </Form>
+          <Form onSubmit={handleFormGirl} id="formGirls">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                required={true}
+                name="password"
+                type="password"
+                placeholder="Contraseña"
+                onChange={changeForm}
+              />
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button variant="danger" type="submit">
+                Entrar
+              </Button>
+            </div>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -54,4 +61,4 @@ export default () => {
       </Modal>
     </>
   );
-}
+};

a4f121a21a1f5876b11e90e6299f1c75990d5ea8
Samuel Suarez <sdsuarez@saludelectronica.com>
2022-08-07 00:05:21 -0500

    boton de whatsapp

--- src/components/navbarComponent.js
+++ src/components/navbarComponent.js
@@ -1,34 +1,45 @@
-import {default as Title} from './titleComponent';
-import { Button, Container, Navbar, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import ModalComponent from './modalComponent';
-import whatsapp from '../whatsapp.png';
+import { default as Title } from "./titleComponent";
+import { Button, Container, Navbar, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ModalComponent from "./modalComponent";
+import whatsapp from "../whatsapp.png";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 export default (props) => {
-
   const redirectLinkWha = (url) => {
-    window.open(url)
-  }
+    window.open(url);
+  };
 
   return (
     <>
-      <Navbar className='navbar'>
+      <Navbar className="navbar">
         <Container>
-          <Title title={'Latin hot web'}></Title>
-          <div className='numbersWhatsapp'>
-            <span><Image src={whatsapp} className='iconWhatsapp'></Image></span>
-            <Button variant='link' onClick={e => redirectLinkWha('https://wa.link/gybwz3')}><p>+1 (408) 639-1277</p></Button>
-            <Button variant='link' onClick={e => redirectLinkWha('https://wa.link/84tgqw')}><p>+1 (669) 499-0487</p></Button>
-            <Button variant='link' onClick={e => redirectLinkWha('https://wa.link/4drpk0')}><p>+1 (209) 450-4558</p></Button>
+          <Title title={"Latin hot web"}></Title>
+          <div className="numbersWhatsapp">
+            <Dropdown className="d-inline mx-2" autoClose="inside">
+              <div className="divWhatsapp">
+                <Dropdown.Toggle id="dropdown-autoclose-inside" variant="danger">
+                      <span><Image src={whatsapp} className='iconWhatsapp'></Image>whatsapp</span>
+                </Dropdown.Toggle>
+              </div>
+              <Dropdown.Menu>
+                <Dropdown.Item href="https://wa.link/gybwz3">+1 (408) 639-1277</Dropdown.Item>
+                <Dropdown.Item href="https://wa.link/84tgqw">+1 (669) 499-0487</Dropdown.Item>
+                <Dropdown.Item href="https://wa.link/4drpk0">+1 (209) 450-4558</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
           <div hidden={!props.hideModal}>
-            <ModalComponent/>
+            <ModalComponent />
           </div>
           <div hidden={props.hideModal}>
-            <Link to={props.route}><Button variant="danger" >{props.titleButton}</Button></Link>
+            <Link to={props.route}>
+              <Button variant="danger">{props.titleButton}</Button>
+            </Link>
           </div>
         </Container>
       </Navbar>
     </>
   );
-}
+};
